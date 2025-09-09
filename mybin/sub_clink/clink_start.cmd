0897179919081aec54cfcc352148bddab3459669
ashi876 <49273367@qq.com>
2025-09-09 19:48:03 +0800

    Update clink_start.cmd

--- mybin/sub_clink/clink_start.cmd
+++ mybin/sub_clink/clink_start.cmd
@@ -2,9 +2,8 @@
 rem 这里是 Clink 启动时自动运行的脚本
 rem 定义我的常用 Doskey 宏
 
-doskey md5=CertUtil -hashfile "$*" MD5
-doskey sha1=CertUtil -hashfile "$*" SHA1
-doskey sha256=CertUtil -hashfile "$*" SHA256
-doskey sha512=CertUtil -hashfile "$*" SHA512
-doskey crc32=CertUtil -hashfile "$*" CRC32
+doskey md5=CertUtil -hashfile $* MD5
+doskey sha1=CertUtil -hashfile $* SHA1
+doskey sha256=CertUtil -hashfile $* SHA256
+doskey sha512=CertUtil -hashfile $* SHA512
 doskey base64=powershell -Command "[convert]::ToBase64String([Text.Encoding]::UTF8.GetBytes(\"$*\"))"
